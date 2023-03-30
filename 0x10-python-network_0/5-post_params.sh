cc3bc92169e30e881ff87507270b94090ff08724
Stephenchuks <stephenchuks@my.uopeople.edu>
2023-03-30 04:16:58 -0700

    Added advanced tasks

--- 0x10-python-network_0/5-post_params.sh
+++ 0x10-python-network_0/5-post_params.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# takes in URL, sends POST request to passed URL, displays body of the response
-curl -sL "$1" -X POST -d "email=hr@holbertonschool.com&subject=I will always be here for PLD"
+# takes in a URL, sends a POST request to the passed URL, and displays the body of the response
+curl -s "$1" -X POST -d "email=test@gmail.com" -d "subject=I will always be here for PLD"

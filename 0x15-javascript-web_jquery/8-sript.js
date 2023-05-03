d55ff0c81398485324f63b2c114cfc2b92b64e24
Stephenchuks <stephenchuks@my.uopeople.edu>
2023-05-02 23:48:01 -0700

    Initial commit

---
+++ 0x15-javascript-web_jquery/8-sript.js
@@ -0,0 +1,9 @@
+$(document).ready(function () {
+  $.get('https://swapi-api.alx-tools.com/api/films/?format=json', function (data) {
+    const movies = data.results;
+    for (let i = 0; i < movies.length; i++) {
+      $('UL#list_movies').append('<li>' + movies[i].title + '</li>');
+    }
+  });
+});
+
